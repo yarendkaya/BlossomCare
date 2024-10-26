127241b457ff2f8a31a335d7a8666d1bf157940a
yarendemirkaya <yarendmrky65@gmail.com>
2024-10-26 17:58:32 +0300

    removed comments

--- app/src/main/java/com/demirkayayaren/blossomcare/data/local/BlossomFavConverter.kt
+++ app/src/main/java/com/demirkayayaren/blossomcare/data/local/BlossomFavConverter.kt
@@ -12,44 +12,4 @@ class BlossomFavConverter {
     fun fromOtherNames(otherNames: List<String>): String {
         return otherNames.joinToString(",")
     }
-
-//    @TypeConverter
-//    fun toScientificNames(scientificNames: String): List<String> {
-//        return scientificNames.split(",")
-//    }
-//
-//    @TypeConverter
-//    fun fromScientificNames(scientificNames: List<String>): String {
-//        return scientificNames.joinToString(",")
-//    }
-//
-//    @TypeConverter
-//    fun toSunlight(sunlight: String): List<String> {
-//        return sunlight.split(",")
-//    }
-//
-//    @TypeConverter
-//    fun fromSunlight(sunlight: List<String>): String {
-//        return sunlight.joinToString(",")
-//    }
-//
-//    @TypeConverter
-//    fun toCycle(cycle: String): List<String> {
-//        return cycle.split(",")
-//    }
-//
-//    @TypeConverter
-//    fun fromCycle(cycle: List<String>): String {
-//        return cycle.joinToString(",")
-//    }
-//
-//    @TypeConverter
-//    fun toWatering(watering: String): List<String> {
-//        return watering.split(",")
-//    }
-//
-//    @TypeConverter
-//    fun fromWatering(watering: List<String>): String {
-//        return watering.joinToString(",")
-//    }
 }
